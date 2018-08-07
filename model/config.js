a3fe33baa0f71724ec3530261b1a31321fd2a62c
dyyaoT <646617513@qq.com>
2018-08-08 02:54:57 +0800

    数据库云端迁移

--- model/config.js
+++ model/config.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/getBook', {useNewUrlParser: true});
+mongoose.connect('mongodb://yao:yjr1923521@116.62.145.106:27017/getBook', {useNewUrlParser: true});
 var db = mongoose.connection;
 db.once("open",() => {
     console.log("数据库连接成功")
 })
+
