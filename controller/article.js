5eb7c8f16aa110edbe16ec15b6b2f0e9c990e1de
dyyaoT <646617513@qq.com>
2018-08-10 02:11:42 +0800

    优化爬虫算法备份

--- controller/article.js
+++ controller/article.js
@@ -2,17 +2,23 @@ const Router = require('koa-router')
 const router = new Router()
 const articleModel = require('../model/articles')
 const titleModel = require('../model/titles')
+const Towxml = require('../util/towxml/main')
+const towxml = new Towxml()
 
 router.get('/article/:id', async ctx => {
     const {id} = ctx.params
-    const data = await articleModel.findOne({titleId: id})
+    let data = await articleModel.findOne({titleId: id})
     const title = await titleModel.findById(data.titleId)
+    // let nodeArr = towxml.toJson(data.content, 'markdown')
+    let nodeArr = towxml.md2wxml(data.content)
+
     console.log(title)
     ctx.body = {
         code: 200,
         data: {
             article: data,
-            title: title.title
+            title: title.title,
+            nodeArr
         }
     }
 })
