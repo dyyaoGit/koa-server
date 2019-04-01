2541fd9d83bc0fc82e8306226e9cd9e05c9d6a25
dyyao <646617513@qq.com>
2019-04-01 09:36:32 +0800

    add cors

--- controller/user.js
+++ controller/user.js
@@ -6,6 +6,11 @@ const userModel = require('../model/user')
 const smsClient = require('../sms/main')
 const validator = require('validator')
 const smsModel = require('../model/sms')
+const cors = require('koa-cors')
+
+router.use(cors({
+
+}))
 
 const jwt = require('jsonwebtoken')
 
